c7e767da0d4c2801bb814936626a3ab91e4587c6
peniel-r <penielruizl@protonmail.com>
2026-01-12 23:03:05 -0600

    fix: re-enable run step in build.zig

--- build.zig
+++ build.zig
@@ -40,14 +40,13 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(exe);
 
-    // Temporarily disable run step due to Windows build issue
-    // const run_step = b.step("run", "Run the app");
-    // const run_cmd = b.addRunArtifact(exe);
-    // run_step.dependOn(&run_cmd.step);
+    const run_step = b.step("run", "Run the app");
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
 
-    // run_cmd.step.dependOn(b.getInstallStep());
+    run_cmd.step.dependOn(b.getInstallStep());
 
-    // if (b.args) |args| {
-    //     run_cmd.addArgs(args);
-    // }
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
 }
